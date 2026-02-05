de1294a5c27e1878c01650b661f8183acb942b9b
王 艺颖 <2080891051@qq.com>
2026-02-05 21:03:57 +0800

    20260205

---
+++ gulimail-ware/src/main/java/com/atguigu/gulimail/ware/GulimailWareApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimail.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+
+@SpringBootApplication
+public class GulimailWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailWareApplication.class, args);
+    }
+
+}
